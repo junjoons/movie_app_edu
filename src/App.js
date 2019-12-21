8ca820e01d5b544ec3a4b5e9a42f3d182104d3b8
junjoons <martin.junyoung@outlook.com>
2019-12-21 15:10:42 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import Itc from './Itc'
+
+function Itc({teacher}) 
+{
+  return (
+    <h1>ITC loves {teacher}</h1>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
-      <Itc />
+      <Itc teacher="Bonnie"/>
+      <Itc teacher="Will"/>
+      <Itc teacher="KHJ"/>
+      <Itc teacher="HSN"/>
     </div>
   );
 }
--- src/Itc.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-function Itc()
-{
-    return <h1>ITC xD</h1>
-}
-
-export default Itc;
