4105aa2d099a31e429067b8fba578294b18ba3e3
junjoons <martin.junyoung@outlook.com>
2019-12-30 21:31:59 +0900

    #3.3 Planning the Movie Component

--- src/App.js
+++ src/App.js
@@ -3,36 +3,19 @@ import PropTypes from "prop-types";
 
 class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
-
-  plus = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
-  };
-
   componentDidMount() {
-    console.log("component rendered");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
-
-  componentDidUpdate() {
-    console.log("component was updated!");
-  }
-
-  componentWillUnmount() {
-    console.log("Ill be back...");
-  }
-
   render() {
-    console.log("Im rendering");
+    const { isLoading } = this.state;
     return (
       <div>
-        <h1>the number is {this.state.count}</h1>
-        <button onClick={this.plus}>Plus</button>
-        <button onClick={this.minus}>Minus</button>
+        <h1>{isLoading ? "Loading" : "we are ready"}</h1>
       </div>
     );
   }
