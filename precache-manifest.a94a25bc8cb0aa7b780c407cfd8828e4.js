411f34cf614310068a57e8df3196509fc158cf84
junjoons <martin.junyoung@outlook.com>
2020-01-07 11:56:30 +0900

    Updates

---
+++ precache-manifest.a94a25bc8cb0aa7b780c407cfd8828e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3470ff608cfd43a011db646e1f28652",
+    "url": "/movie_app_edu/index.html"
+  },
+  {
+    "revision": "4d3234b27dd3b96b59fd",
+    "url": "/movie_app_edu/static/css/main.d02acac5.chunk.css"
+  },
+  {
+    "revision": "4991106e30b767e8334c",
+    "url": "/movie_app_edu/static/js/2.d1748440.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/movie_app_edu/static/js/2.d1748440.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4d3234b27dd3b96b59fd",
+    "url": "/movie_app_edu/static/js/main.63a60225.chunk.js"
+  },
+  {
+    "revision": "6ad4a2ddee229d74441f",
+    "url": "/movie_app_edu/static/js/runtime-main.d027802c.js"
+  }
+]);
