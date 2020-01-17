13746ce5a00413c9d42108840b20bcc981ef944e
junjoons <martin.junyoung@outlook.com>
2020-01-17 14:34:33 +0900

    #6.2.2 Building the Navigation

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./About.css";
 
 function About() {
-  return <h1>ABOUT!</h1>;
+  return (
+    <div className="about__container">
+      <span>
+        do not go gentle into that good night. rage, rage against the dying of
+        the light...
+      </span>
+      <span>--Dylan Thomas</span>
+    </div>
+  );
 }
 
 export default About;
